20b441936a9ab57d650161847af8fa6ee46f7e45
MikkCZ <michal.stanke@mikk.cz>
2015-03-22 11:06:26 +0100

    Lepší formátování výstupu

--- wp-rajce-galerie.php
+++ wp-rajce-galerie.php
@@ -23,23 +23,22 @@ function show_rajce_profile($atts, $content = NULL) {
 		$atts
 	);
 	if ($atts['uzivatel'] == NULL) {
-		return '<!-- Nebyl zadán žádný uživatel ('.$plugin_data['Name'].'). -->';
+		return sprintf('<!-- Nebyl zadán žádný uživatel (%s). -->', $plugin_data['Name']);
 	}
+
 	$username = strtolower($atts['uzivatel']);
-
 	if(!ctype_alnum($username)) {
-		return '<!-- Zadaný uživatel je neplatný ('.$plugin_data['Name'].'). -->';
+		return sprintf('<!-- Zadaný uživatel je neplatný (%s). -->', $plugin_data['Name']);
 	}
 
-	$rss_url = 'http://' . $username . '.rajce.idnes.cz/?rss=news';
-	$headers = get_headers($rss_url);
-	if (substring_in_array(' 404 Not Found', $headers)) {
-		return '<!-- Zadaný uživatel neexistuje ('.$plugin_data['Name'].'). -->';
-	}
+	$rss_url = sprintf('http://%s.rajce.idnes.cz/?rss=news', $username);
 
-	$in_cache = trailingslashit(WP_CONTENT_DIR).'wp-rajce-galerie-cache/'.$username.'.rss';
+	$in_cache = trailingslashit(WP_CONTENT_DIR).sprintf('wp-rajce-galerie-cache/%s.rss', $username);
 	if( !is_file($in_cache) || !(time()-filemtime($in_cache) < 7200) ) {
 		$rss_file_content = file_get_contents($rss_url);
+		if(substring_in_array(' 404 Not Found', $http_response_header)) {
+			return sprintf('<!-- Zadaný uživatel neexistuje (%s). -->', $plugin_data['Name']);
+		}
 		if(substring_in_array(' 200 OK', $http_response_header)) {
 			if(!file_exists(dirname($in_cache))) {
 				mkdir(dirname($in_cache));
@@ -80,14 +79,13 @@ function substring_in_array($substring, $array) {
 function format_output($headline, $albums_from_rss, $show_titles) {
 	$output = '<div class="wp-rajce-galerie">' . PHP_EOL;
 	if ($headline != NULL) {
-		$output .= '<h3>' . $headline . '</h3>' . PHP_EOL;
+		$output .= sprintf('<h3>%s</h3>', $headline) . PHP_EOL;
 	}
 	foreach ($albums_from_rss as $album) {
-		$output .= '<div class="rajce-album">';
-		$output .= '<a href="' . $album['album_url'] . '">';
-		$output .= '<img src="' . $album['thumbnail_url'] . '" alt="' . $album['title'] . '" class="rajce-thumbnail">';
+		$output .= '<div class="album">';
+		$output .= sprintf('<a href="%s">', $album['album_url']);
+		$output .= sprintf('<img src="%1$s" alt="%2$s" class="thumbnail">', $album['thumbnail_url'], $album['title']);
 		if ($show_titles) {
-			$output .= '<br>';
 			$output .= $album['title'];
 		}
 		$output .= '</a>';
